a5d4249eaa36815eefb0d1902d87c93f2995edc4
Sumit-cs <sumitbhalsing12@gmail.com>
2026-07-21 21:23:25 +0530

    Add files via upload

---
+++ C Programming/Practice/LB Class/program71.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt=0;
+     iCnt=1; 
+    while(iCnt<=5) //while manje joprynt //conditon lihaychi
+    {
+        printf("Jay geanesh \n");
+        iCnt++;
+    }
+    return 0;
+}
---
+++ C Programming/Practice/LB Class/program72.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt=0;
+     iCnt=1; 
+     //while manje joprynt //conditon lihaychi
+     do // kara
+    {
+        printf("Jay ganesh \n");
+        iCnt++;
+    }while(iCnt<=5); //semicolon laagto in do while case
+    return 0;
+}
