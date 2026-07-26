f91d639b96138cbf1a4d686aa500c5b5fca4aafe
Sumit-cs <sumitbhalsing12@gmail.com>
2026-07-26 22:05:12 +0530

    Add files via upload

---
+++ C Programming/Practice/LB Class/program144.c
@@ -0,0 +1,51 @@
+#include<stdlib>
+
+void Update(int Arr[],int iSize)
+{
+    int iCnt =0;
+    for(iCnt =0;iCnt <iSize;iCnt++)   
+    {
+        Arr[iCnt]++;
+    }
+
+    printf("Array elements after function call:\n");
+    for(iCnt =0;iCnt < 5;iCnt++)   
+    {
+        printf("%d\n",Arr[iCnt]);
+    }
+
+}
+int main()
+{
+    int Brr[]={10,20,30,40,50};
+    int iCnt =0;
+
+    printf("Array elements before function call\n")
+
+    for(iCnt =0;iCnt < 5;iCnt++)
+    {
+        printf("%d\n",Brr[Cnt]);
+    }
+    Update(Brr,5);
+
+    return 0;
+}
+//no
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
